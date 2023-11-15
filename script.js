a619f08880ad640be4621edd85fc6ad849357396
Jason <JasonArriaza10@gnail.com>
2023-11-15 17:54:42 -0600

    added the prompts to pop up when clicked by button

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // Assignment code here
 function generatePassword(){
-  prompt('Hello there');
+  var upperCaseValue = window.confirm('Would you like to add uppercase letters to your password?');
+  var lowerCaseValue = window.confirm('Would you like to add lowercase letters to you password?');
+  var numberValue = window.confirm('Would you like to add numbers to your password?');
+  var symbolsValue = window.confirm('Would you like to add symbols to your password?');
+  var lengthValue = prompt('How long would you like your password to be (Must be in between 8-128 characters)');
 }
 
-
+//generate the lowercase letters
+//generate the uppercase letters
+//generate the symbols
+//Hava a linit for the password
 
 
 // Get references to the #generate element
